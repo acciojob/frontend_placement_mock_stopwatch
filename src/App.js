463181573137c8b73b05c469ab534254ba17b8a9
Mrityunjay3 <44922081+Mrityunjay3@users.noreply.github.com>
2023-01-04 15:22:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <button>Start</button>
+      <button>Pause</button>
+      <button>Reset</button>
+    </div>
+  );
+}
